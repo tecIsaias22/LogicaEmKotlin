65d831ba6df5b34c8ec42f4a25aac8f5336e17b7
tecIsaias22 <tecnologiaisaias22@gmail.com>
2025-07-02 22:17:19 -0300

    Add files via upload

--- Variaveis/Aula1.1.kt
+++ Variaveis/Aula1.1.kt
@@ -2,6 +2,24 @@ package Variaveis
 
 fun main() {
 
+    /*
+ * Numeros
+ * Tipo tamanho (bits) faixa de valores
+ *
+ * Byte | UByte      8      - 126 A 127                              | 0 A 255 (u) unsigned
+ * Short | UShort    16     - 32_768 A 32_767                        | o A 655535 (u)
+ * Int | UInt        32     - 2_147_483_648 A 2_147_483_647          | 0 A 4294967295 (u)
+ * Long | ULong      64     - 9 223_372_036_854_775_807 A
+                              9 223_372_036_854_775_808              | 0 A 18446744073709551615 A 9 (u)
+
+ * Float             32       Aproximadamente +3.4 X 10^38
+ * Double            64       Aproximadamente +1.7 X 10^308
+
+
+ * Boolean           8
+ * char              16
+*/
+
     var Idade: Byte
     Idade = 22
     var Idade2: Byte = 22
--- Variaveis/Aula1.2.kt
+++ Variaveis/Aula1.2.kt
@@ -1,21 +1,28 @@
 package Variaveis
 
+import java.util.Properties
+
+const val s2: String = "Maria"
+
 fun main(){
-    /*
-    * Numeros
-    * Tipo tamanho (bits) faixa de valores
-    *
-    * Byte | UByte      8      - 126 A 127                              | 0 A 255 (u) unsigned
-    * Short | UShort    16     - 32_768 A 32_767                        | o A 655535 (u)
-    * Int | UInt        32     - 2_147_483_648 A 2_147_483_647          | 0 A 4294967295 (u)
-    * Long | ULong      64     - 9 223_372_036_854_775_807 A
-                                 9 223_372_036_854_775_808              | 0 A 18446744073709551615 A 9 (u)
 
-    * Float             32       Aproximadamente +3.4 X 10^38
-    * Double            64       Aproximadamente +1.7 X 10^308
+    var s5 = "Matheus"
+    println(s5)
+    s5 = "Carlos"
+    println(s5)
 
+    val s1 : String ="Isaias"
+    println(s1)
 
-    * Boolean           8
-    * char              16
-  */
+}
+
+class Exemplo {
+    companion object{
+        const val s3 ="Ana"
+    }
+}
+class Exemplu {
+    companion object{
+        val s4 = Properties().propertyNames()
+    }
 }
