25ebf450262415469ed8c77f70c2fc8be5869040
isaias <tecnologiaisaias22@gmail.com>
2025-07-16 22:14:22 -0300

    Add files via upload

--- Variaveis/Aula1.0.kt
+++ Variaveis/Aula1.0.kt
@@ -76,15 +76,6 @@ fun main() {
  */
 
 
-    val idade: Int
-    idade = 10
-    if(idade == 10){
-        println("SIM")
-    }else{
-        println("NAO")
-
-    }
-
     /*
     // rem() mod() -> resto de uma divisao
     println(10%2)
