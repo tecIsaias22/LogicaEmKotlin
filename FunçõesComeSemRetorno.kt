d29529d5079b50d2774b955b4b70e2dff37a77d0
isaias <tecnologiaisaias22@gmail.com>
2025-10-07 20:38:00 -0300

    Add files via upload

---
+++ FunçõesComeSemRetorno.kt
@@ -0,0 +1,19 @@
+// Com retorno ou sem retorno
+fun Somar(): Int{
+    /*
+    sem retorno
+    println(5+5)
+     */
+    return 10
+}
+
+fun main(){
+    val retorno = Somar()
+    println(retorno * 2)
+    println(retorno + 2)
+    println(retorno - 2)
+    println(retorno / 2)
+
+
+
+}
---
+++ FunçõesInlineFunctionFunçõesEmLinha.kt
@@ -0,0 +1,16 @@
+fun subtrair() = println(10 - 8)
+
+/*
+fun subtrair() = println(10 - 8)
+return 10 - 8
+ */
+
+
+fun main(){
+    subtrair()
+    /*
+    val retorno = subtrair()
+    print(retorno)
+     */
+
+}
